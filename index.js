7528077d4e36ffe286a7bdfce29cedcd7fe67c18
Joy Olago <joydanielle62@gmail.com>
2023-06-20 11:27:31 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solutions in this file
+// // Code your solutions in this file
+
+// //for (let age =30; age < 40; age++){
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+// }
+
+// //const gift = ["teddy bear", "drone", "doll"]
+
+// function wrapGifts(gifts) {
+//     for (let i=0; i< gifts,length; i++){
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//         debugger;
+//     }
+// }
+
+let thankPeople = ["Guadalupe", "Ollie", "Aki"]
+
+
+function writeCards(thankPeople){
+    let emptyArray = []
+    for (let i=0; i <thankPeople.length; i++){
+        emptyArray.push(`Thank you, ${thankPeople[i]}, for the wonderful surprise gift!`)   
+    }
+     return emptyArray;
+}
+console.log(writeCards())
+
+let counter
+function countDown(counter){
+    while (counter >= 0){
+        console.log(counter)
+        counter--
+    }
+}
+countDown(10)
